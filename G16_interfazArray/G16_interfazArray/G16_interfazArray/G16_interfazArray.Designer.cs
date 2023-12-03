561a2c6f276f412ee7b627701cdd334fb8f7fa20
arhka5 <78610558+arhka5@users.noreply.github.com>
2023-12-03 03:25:41 -0800

    Add files via upload

---
+++ G16_interfazArray/G16_interfazArray/G16_interfazArray/G16_interfazArray.Designer.cs
@@ -0,0 +1,219 @@
+﻿namespace G16_interfazArray
+{
+    partial class G16_interfazArray
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(G16_interfazArray));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.G16_textBox = new System.Windows.Forms.TextBox();
+            this.G16_button = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.G16_label5 = new System.Windows.Forms.Label();
+            this.errorProvider1 = new System.Windows.Forms.ErrorProvider(this.components);
+            this.G16_label6 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel3 = new System.Windows.Forms.Panel();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.panel3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.White;
+            this.label1.Font = new System.Drawing.Font("Cascadia Code SemiBold", 11F, System.Drawing.FontStyle.Bold);
+            this.label1.Location = new System.Drawing.Point(55, 38);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(153, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Ingrese un Array";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.White;
+            this.label2.Font = new System.Drawing.Font("Cascadia Code SemiBold", 11F, System.Drawing.FontStyle.Bold);
+            this.label2.Location = new System.Drawing.Point(26, 89);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(63, 20);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Array:";
+            // 
+            // G16_textBox
+            // 
+            this.G16_textBox.Location = new System.Drawing.Point(90, 90);
+            this.G16_textBox.Name = "G16_textBox";
+            this.G16_textBox.Size = new System.Drawing.Size(117, 20);
+            this.G16_textBox.TabIndex = 2;
+            this.G16_textBox.TextChanged += new System.EventHandler(this.G16_textBox_TextChanged);
+            // 
+            // G16_button
+            // 
+            this.G16_button.BackColor = System.Drawing.Color.PeachPuff;
+            this.G16_button.FlatAppearance.BorderSize = 0;
+            this.G16_button.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.G16_button.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.G16_button.Font = new System.Drawing.Font("Cascadia Code SemiBold", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.G16_button.Location = new System.Drawing.Point(81, 128);
+            this.G16_button.Name = "G16_button";
+            this.G16_button.Size = new System.Drawing.Size(113, 31);
+            this.G16_button.TabIndex = 3;
+            this.G16_button.Text = "Calcular";
+            this.G16_button.UseVisualStyleBackColor = false;
+            this.G16_button.Click += new System.EventHandler(this.G16_button_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.White;
+            this.label3.Font = new System.Drawing.Font("Cascadia Code SemiBold", 11F, System.Drawing.FontStyle.Bold);
+            this.label3.Location = new System.Drawing.Point(13, 180);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(171, 20);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Array Desordenado:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.White;
+            this.label4.Font = new System.Drawing.Font("Cascadia Code SemiBold", 11F, System.Drawing.FontStyle.Bold);
+            this.label4.Location = new System.Drawing.Point(11, 250);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(162, 20);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Array Ascendente:";
+            // 
+            // G16_label5
+            // 
+            this.G16_label5.AutoSize = true;
+            this.G16_label5.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.G16_label5.Font = new System.Drawing.Font("Cascadia Code", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.G16_label5.ForeColor = System.Drawing.Color.Black;
+            this.G16_label5.Location = new System.Drawing.Point(3, 0);
+            this.G16_label5.Name = "G16_label5";
+            this.G16_label5.Size = new System.Drawing.Size(0, 20);
+            this.G16_label5.TabIndex = 6;
+            // 
+            // errorProvider1
+            // 
+            this.errorProvider1.ContainerControl = this;
+            // 
+            // G16_label6
+            // 
+            this.G16_label6.AutoSize = true;
+            this.G16_label6.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.G16_label6.Font = new System.Drawing.Font("Cascadia Code SemiBold", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.G16_label6.ForeColor = System.Drawing.Color.Black;
+            this.G16_label6.Location = new System.Drawing.Point(3, 0);
+            this.G16_label6.Name = "G16_label6";
+            this.G16_label6.Size = new System.Drawing.Size(0, 20);
+            this.G16_label6.TabIndex = 7;
+            // 
+            // panel1
+            // 
+            this.panel1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("panel1.BackgroundImage")));
+            this.panel1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.panel1.Controls.Add(this.panel3);
+            this.panel1.Controls.Add(this.panel2);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.label4);
+            this.panel1.Controls.Add(this.G16_button);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Location = new System.Drawing.Point(-1, -2);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(257, 481);
+            this.panel1.TabIndex = 8;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.panel2.Controls.Add(this.G16_label5);
+            this.panel2.Location = new System.Drawing.Point(43, 213);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(184, 22);
+            this.panel2.TabIndex = 8;
+            // 
+            // panel3
+            // 
+            this.panel3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.panel3.Controls.Add(this.G16_label6);
+            this.panel3.Location = new System.Drawing.Point(43, 282);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(184, 22);
+            this.panel3.TabIndex = 9;
+            // 
+            // G16_interfazArray
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Peru;
+            this.ClientSize = new System.Drawing.Size(255, 468);
+            this.Controls.Add(this.G16_textBox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "G16_interfazArray";
+            this.Text = "G16 Interfaz Array ";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.panel3.ResumeLayout(false);
+            this.panel3.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox G16_textBox;
+        private System.Windows.Forms.Button G16_button;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label G16_label5;
+        private System.Windows.Forms.ErrorProvider errorProvider1;
+        private System.Windows.Forms.Label G16_label6;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Panel panel2;
+    }
+}
+
---
+++ G16_interfazArray/G16_interfazArray/G16_interfazArray/G16_interfazArray.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.Rebar;
+
+namespace G16_interfazArray
+{
+    public partial class G16_interfazArray : Form
+    {
+        public G16_interfazArray()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e) // Inicia el Form 
+        {
+            G16_button.Enabled = false; // El boton estara desactivado hasta que se cumplan las condiciones
+        }
+
+        private void G16_button_Click(object sender, EventArgs e) // Método que actua al hacer click
+        {
+
+            // Obtener el texto del G16_textBox y dividirlo en números
+            string numerosStr = G16_textBox.Text;
+            // Convertir los números de string a int
+            int[] numeros = numerosStr.Select(c => int.Parse(c.ToString())).ToArray();
+            // Desordenar el array
+            DesordenarArray(numeros);
+            // Mostrar el array desordenado en G16_label5
+            G16_label5.Text = string.Join(" ", numeros);
+
+            // Ordenar el array
+            // Array.Sort(numeros);
+            // Llamar al método de ordenación Quicksort
+            G16_Ordenar(numeros, 0, numeros.Length - 1);
+            // Mostrar el array ordenado en G16_label5
+            G16_label6.Text = string.Join(" ", numeros);
+        }
+
+        private void G16_Ordenar(int[] array, int low, int high)
+        {
+            if (low < high)
+            {
+                // Encuentra la posición del pivote, el elemento en su posición final en la ordenación
+                int pi = G16_Particion(array, low, high);
+
+                // Ordena recursivamente los elementos antes y después del pivote
+                G16_Ordenar(array, low, pi - 1);
+                G16_Ordenar(array, pi + 1, high);
+            }
+        }
+
+        private int G16_Particion(int[] array, int low, int high)
+        {
+            int pivot = array[high];
+            int i = low - 1;
+
+            for (int j = low; j < high; j++)
+            {
+                if (array[j] < pivot)
+                {
+                    i++;
+
+                    // Intercambia array[i] y array[j]
+                    int temp = array[i];
+                    array[i] = array[j];
+                    array[j] = temp;
+                }
+            }
+
+            // Intercambia array[i+1] y array[high] (o el pivote)
+            int tempPivot = array[i + 1];
+            array[i + 1] = array[high];
+            array[high] = tempPivot;
+            return i + 1;
+        }
+
+        private void DesordenarArray<T>(T[] array)
+        {
+            Random rand = new Random();
+            int n = array.Length;
+
+            // Desordenar el array usando el algoritmo de Fisher-Yates
+            for (int i = n - 1; i > 0; i--)
+            {
+                int j = rand.Next(0, i + 1);
+                T temp = array[i];
+                array[i] = array[j];
+                array[j] = temp;
+            }
+        }
+
+        private void G16_textBox_TextChanged(object sender, EventArgs e)
+        {
+            controlBoton(); // Al cambio en el texto se ejecuta el control
+
+        }
+
+        private void controlBoton()
+        {
+            // Se verifica el texto en el G16_textBox
+            if (G16_textBox.Text.Trim() != string.Empty && G16_textBox.Text.All(char.IsNumber ) && G16_textBox.Text.Trim().Length < 10)
+            {
+                G16_button.Enabled = true; // Si el G16_textBox cumple el boton se activa
+                errorProvider1.SetError(G16_textBox, ""); 
+
+            }
+            else 
+            { 
+            if ((G16_textBox.Text.All(char.IsLetter)))
+                {
+                errorProvider1.SetError(G16_textBox, "El array sólo debe contener numeros");
+                }
+            else {
+                errorProvider1.SetError(G16_textBox, "Debe introducir un array de máximo 9 cifras");
+                }
+            G16_button.Enabled = false; // Si el G16_textBox no cumple el boton se desactiva
+                G16_button.Focus();
+        }   }
+    }
+}
